22c0f46644679299288e71c3fc41bb60198f2b8b
Skarrg <orestes.moiran@gmail.com>
2018-11-01 21:31:19 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDfldE2pP1wEjouj1s4K53v3yD1QhLUiHA",
+    apiKey: "placeholder",
     authDomain: "rpsonline-3fc84.firebaseapp.com",
     databaseURL: "https://rpsonline-3fc84.firebaseio.com",
     projectId: "rpsonline-3fc84",
