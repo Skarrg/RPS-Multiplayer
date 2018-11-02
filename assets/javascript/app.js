b1d826a9343af4871b77bcc322201baae69712b0
Skarrg <orestes.moiran@gmail.com>
2018-11-01 19:49:14 -0700

    intial placeholder and hookups for everything

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDfldE2pP1wEjouj1s4K53v3yD1QhLUiHA",
+    authDomain: "rpsonline-3fc84.firebaseapp.com",
+    databaseURL: "https://rpsonline-3fc84.firebaseio.com",
+    projectId: "rpsonline-3fc84",
+    storageBucket: "rpsonline-3fc84.appspot.com",
+    messagingSenderId: "1020541368893"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
